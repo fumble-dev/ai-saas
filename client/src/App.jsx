df313178575d421791c5b6dc9c4898baa9b9b53d
Udanth <udanth.work@gmail.com>
2025-07-16 16:53:44 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Layout from './pages/Layout.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import WriteArticle from './pages/WriteArticle.jsx'
+import BlogTitles from './pages/BlogTitles.jsx'
+import GenerateImage from './pages/GenerateImages.jsx'
+import RemoveBackground from './pages/RemoveBackground.jsx'
+import RemoveObject from './pages/RemoveObject.jsx'
+import ReviewResume from './pages/ReviewResume.jsx'
+import Community from './pages/Community.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/ai' element={<Layout/>} >
+            <Route index element={<Dashboard/>} />
+            <Route path='write-article' element={<WriteArticle/>} />
+            <Route path='blog-titles' element={<BlogTitles/>} />
+            <Route path='remove-background' element={<RemoveBackground/>} />
+            <Route path='remove-object' element={<RemoveObject/>} />
+            <Route path='review-resume' element={<ReviewResume/>} />
+            <Route path='generate-images' element={<GenerateImage/>} />
+            <Route path='community' element={<Community/>} />
+        </Route>
+        
+      </Routes>
+    </div>
+  )
+}
+
+export default App
