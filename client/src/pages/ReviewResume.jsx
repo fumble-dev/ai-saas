57d9b249f17af79b6da95641f5dacd9b6a8c5ed2
Udanth <udanth.work@gmail.com>
2025-07-21 12:31:43 +0530

    completed resume review

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -1,11 +1,41 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+import Markdown from "react-markdown";
 
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 const ReviewResume = () => {
-
   const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+
+  const { getToken } = useAuth();
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true);
+      const formData = new FormData();
+      formData.append("resume", input);
+      setContent("");
+
+      const { data } = await axios.post("/api/ai/resume-review", formData, {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (data.success) {
+        setContent(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setLoading(false);
   };
 
   return (
@@ -26,24 +56,42 @@ const ReviewResume = () => {
           className="w-full p-2 text-sm border rounded outline-none "
           required
         />
-        <p className="text-xs text-gray-500 font-light mb-2">Upload in PDF format only</p>
+        <p className="text-xs text-gray-500 font-light mb-2">
+          Upload in PDF format only
+        </p>
         <button
+          disabled={loading || !input}
           type="submit"
-          className="w-full p-2 text-sm border rounded cursor-pointer hover:bg-gray-100 mt-5"
+          className={`w-full p-2 text-sm border rounded flex justify-center items-center gap-2 transition-all ${
+            loading || !input
+              ? "opacity-50 cursor-not-allowed"
+              : "hover:bg-gray-100"
+          }`}
         >
-          Review Resume
+          {loading && (
+            <span className="w-4 h-4 border-2 border-t-transparent border-gray-600 rounded-full animate-spin"></span>
+          )}
+          {loading ? "Reviewing..." : "Review Resume"}
         </button>
       </form>
 
       {/* Right Column */}
       <div className="w-full max-w-lg p-4 bg-white border rounded flex flex-col">
         <h2 className="text-base mb-4">Analysis Results</h2>
-        <div className="flex-1 flex items-center justify-center text-sm text-gray-600">
-          <p>Upload resume to get started...</p>
-        </div>
+        {!content ? (
+          <div className="flex-1 flex items-center justify-center text-sm text-gray-600">
+            <p>Upload resume to get started...</p>
+          </div>
+        ) : (
+          <div className="mt-3 h-134 overflow-y-auto text-sm text-slate-600">
+            <div className="reset-tw">
+              <Markdown>{content}</Markdown>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export default ReviewResume
+export default ReviewResume;
